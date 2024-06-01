b51cd143036b084b2182abe61814c76f251cbf6c
avishkakavindu <avishkakavindud@gmail.com>
2024-06-01 21:10:40 +0530

    TypeOrm configs added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // TypeOrmModule.forRootAsync allows dynamic database configuration using environment variables
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD') ?? 'password',
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts, .js}'], // Include entities from all modules
+        synchronize: true, // Automatically syncs the database schema with the entity definitions; useful for development but should be false in production
+      }),
+      inject: [ConfigService], // Injects the ConfigService so it can be used in the useFactory function
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT') ?? 4001;
+
+  await app.listen(port);
+  console.log(`============================================`);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`============================================`);
 }
 bootstrap();
